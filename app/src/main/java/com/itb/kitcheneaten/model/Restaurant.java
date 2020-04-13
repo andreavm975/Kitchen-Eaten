f8c0a1eca1e39eff03a3d05b863f22f75f8c1953
Miquel <miloki27@outlook.com>
2020-04-13 16:11:08 +0200

    Clase modelo creada

---
+++ app/src/main/java/com/itb/kitcheneaten/model/Restaurant.java
@@ -0,0 +1,51 @@
+package com.itb.kitcheneaten.model;
+
+public class Restaurant {
+
+    private String name;
+    private String address;
+    private String tel;
+    private int nTables;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(String name, String address, String tel, int nTables) {
+        this.name = name;
+        this.address = address;
+        this.tel = tel;
+        this.nTables = nTables;
+    }
+
+    public String getNombre() {
+        return name;
+    }
+
+    public void setNombre(String nombre) {
+        this.name = nombre;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getnTables() {
+        return nTables;
+    }
+
+    public void setnTables(int nTables) {
+        this.nTables = nTables;
+    }
+}
