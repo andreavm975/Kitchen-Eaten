ec2ef93ce7e6e33fa95f7d4f888a77dfa7d4ded1
andrea <andrea.valcarcel.7e2@itb.cat>
2020-04-14 17:29:42 +0200

    Navigation in process

--- app/src/main/java/com/itb/kitcheneaten/fragments/RestaurantDetailFragment.java
+++ app/src/main/java/com/itb/kitcheneaten/fragments/RestaurantDetailFragment.java
@@ -7,17 +7,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.itb.kitcheneaten.R;
 
+import java.util.Objects;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RestaurantDetailFragment extends Fragment {
 
     private RestaurantDetailViewModel mViewModel;
+    private String name;
 
     public static RestaurantDetailFragment newInstance() {
         return new RestaurantDetailFragment();
@@ -33,6 +40,9 @@ public class RestaurantDetailFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
+        if(getArguments()!=null){
+            name = RestaurantDetailFragmentArgs.fromBundle(getArguments().);
+        }
     }
 
     @Override
@@ -42,4 +52,9 @@ public class RestaurantDetailFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @OnClick(R.id.btnReservation)
+    public void onReservationClicked(){
+        NavDirections navigation = RestaurantDetailFragmentDirections.actionRestaurantDetailFragmentToTableReserveFragment(name);
+        Navigation.findNavController(Objects.requireNonNull(getView())).navigate(navigation);
+    }
 }
