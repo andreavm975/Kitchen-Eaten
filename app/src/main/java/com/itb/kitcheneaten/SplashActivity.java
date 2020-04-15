5369db7d581c4ace1e2f3f55562797aacdcffeb2
Miquel <miloki27@outlook.com>
2020-04-15 21:49:04 +0200

    Pantalla de presentacion

---
+++ app/src/main/java/com/itb/kitcheneaten/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.itb.kitcheneaten;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private Handler myHandler;
+    private Runnable myRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        };
+
+        myHandler = new Handler();
+
+        myHandler.postDelayed(myRunnable, 3000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (myHandler != null && myRunnable != null) {
+            myHandler.removeCallbacks(myRunnable);
+        }
+    }
+}
